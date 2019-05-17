53ad3d2fd2e461b71101605c9ee3dbd1154c7f5c
Trần văn Lộc <01633467573>
2019-05-17 14:36:22 +0700

    ok back to main

--- TaskUser/ViewsModels/ProductViewsModels/ProductViewsModels.cs
+++ TaskUser/ViewsModels/ProductViewsModels/ProductViewsModels.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Http;
 
 namespace TaskUser.ViewsModels.ProductViewsModels
 {
@@ -12,6 +13,7 @@ namespace TaskUser.ViewsModels.ProductViewsModels
         public int BrandId { get; set; }
         public int CategoryId { get; set; }
         public int  ModelYear { get; set; }
+        [DisplayFormat(DataFormatString = "{0:0}", ApplyFormatInEditMode = true)]
         public decimal ListPrice { get; set; }
         public string Picture { get; set; }
         public IFormFile  PictureFile { get; set; }
